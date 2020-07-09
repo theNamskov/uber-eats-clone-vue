aac8b03009ce649e4ea1d7a94d048250adf98792
thenamskov <thenamskov@localhost.localdomain>
2020-07-09 15:44:32 -0500

    feat: added new component

---
+++ src/components/BlueBackground.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+/* eslint-disable indent */
+export default {
+    name: 'blue-background',
+    data() {
+        return {
+            msg: 'I have a blue background',
+        };
+    },
+};
+</script>
+
+<style scoped>
+    body {
+        background: blue;
+    }
+    h1 {
+        color: orange;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import BlueBackground from '@/components/BlueBackground';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+    },
+    {
+      path: '/this-guy-too',
+      name: 'BlueBackground',
+      component: BlueBackground,
     },
   ],
 });
